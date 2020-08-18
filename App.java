59e231299a3a6e7fd13ef781254b913a556d9c52
Maximiliano Eschoyez <31444199+meschoyez@users.noreply.github.com>
2020-08-18 10:47:46 -0300

    Estructura if para control de radio en el circulo
    Tambien se implementa el color a utilizar

--- App.java
+++ App.java
@@ -3,44 +3,42 @@ import java.awt.Color;
 public class App {
     
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        Circulo miCirculo = new Circulo (45);
-        Circulo otroCirculo = new Circulo (98);
-
-        System.out.println(miCirculo.getRadio());
-        System.out.println(otroCirculo.getRadio());
-        
-        miCirculo.setRadio(60);
-        miCirculo.setX(250);
-        miCirculo.setY(250);
-        System.out.println(miCirculo.getRadio());
-
+        Circulo circulo1 = new Circulo (100);
+        Circulo circulo2 = new Circulo (300);
+        Circulo circulo3 = new Circulo (600);
         // Codificar acciones parecidas para la 
         // clase Rectangulo
         // Rectangulo ...
 
+        circulo1.setX(250);
+        circulo1.setY(250);
 
-        Canvas miVentana = new Canvas("Hola Ventana", 500, 500);
+        circulo2.setX(150);
+        circulo2.setY(150);
+
+        Canvas miVentana = new Canvas("Hola Ventana", 800, 600);
         miVentana.setVisible(true);
 
-        miVentana.setColorDeLapiz(Color.BLUE);
+        // Dibujamos el primer Circulo
+        // miVentana.setColorDeLapiz(Color.GREEN);
+        miVentana.setColorDeLapiz(circulo3.getColor());
+        miVentana.rellenarCirculo(circulo3.getX(), circulo3.getY(), circulo3.getDiametro());
 
-        miVentana.rellenarCirculo(miCirculo.getX(), miCirculo.getY(), miCirculo.getDiametro());
+        miVentana.setColorDeLapiz(circulo2.getColor());
+        miVentana.rellenarCirculo(circulo2.getX(), circulo2.getY(), circulo2.getDiametro());
 
-        miVentana.setColorDeLapiz(Color.GREEN);
-
-        miVentana.rellenarCirculo(otroCirculo.getX(), otroCirculo.getY(), otroCirculo.getDiametro());
+        miVentana.setColorDeLapiz(circulo1.getColor());
+        miVentana.rellenarCirculo(circulo1.getX(), circulo1.getY(), circulo1.getDiametro());
 
         // Dibujar un rectangulo de color ROJO (RED)
         // rellenarRectangulo(int xPos, int yPos, int ancho, int alto)
 
-        miVentana.espera(30000);
-        miVentana.borrarCirculo(miCirculo.getX(), miCirculo.getY(), miCirculo.getDiametro());
-        // miCirculo.setX(250);
-        miCirculo.setY(150);
-        miVentana.setColorDeLapiz(Color.BLUE);
-        miVentana.rellenarCirculo(miCirculo.getX(), miCirculo.getY(), miCirculo.getDiametro());
+        // miVentana.espera(30000);
+        // miVentana.borrarCirculo(circulo1.getX(), circulo1.getY(), circulo1.getDiametro());
+        // // circulo1.setX(250);
+        // circulo1.setY(150);
+        // miVentana.setColorDeLapiz(Color.BLUE);
+        // miVentana.rellenarCirculo(circulo1.getX(), circulo1.getY(), circulo1.getDiametro());
 
     }
 }
--- Circulo.java
+++ Circulo.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 // Modificador visibilidad - palabra reservada - Nombre de la clase
 // Identificadores son CamelCase
 public class Circulo {
@@ -7,7 +9,8 @@ public class Circulo {
     private static final int MAX_RADIO = 1000;
     // static -> campo de la clase
     // final -> hace que sea un valor constante (no modificable - inmutable)
-    private int radio; // int char
+    private Color color ;
+    private int radio = 10; // Valor inicial por si me dan uno fuera de rango
     private int x;
     private int y;
     // Aclaracion -> Datos tipo objeto Integer, Double, Char, Boolean
@@ -15,8 +18,31 @@ public class Circulo {
     // Comportamientos (metodos)
 
     // Constructor -> crear objetos de la clase
-    public Circulo (int r) {
-        radio = r;
+    public Circulo (int radio) {
+        setRadio(radio);
+        determinarColor();
+    }
+
+    /**
+     * Este metodo establece el color del circulo segun
+     * el radio configurado.
+     * 
+     * Si radio < 200  -> tiene color verde
+     * Si radio < 500  -> tiene color amarillo
+     * Si radio >= 500 -> tiene color rojo
+     */
+    private void determinarColor () {
+        if (radio < 200) {
+            setColor(Color.GREEN);
+        }
+        else {
+            if (radio < 500) {
+                setColor(Color.YELLOW);
+            }
+            else {
+                setColor(Color.RED);
+            }
+        }
     }
 
     public double getPerimetro () {
@@ -39,7 +65,9 @@ public class Circulo {
     public void setRadio(int radio) {
         // this = este
         // asignar radio al campo radio de este objeto
-        this.radio = radio;
+        if ((0 < radio) && (radio < MAX_RADIO)) {
+            this.radio = radio;
+        }
     }
 
     public int getX() {
@@ -56,6 +84,14 @@ public class Circulo {
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 
 
